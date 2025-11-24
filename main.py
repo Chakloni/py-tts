701ba2a68db9ddabf99da41f2b21c05c73a9cd35
Zakloni <edjovilellaca@ittepic.edu.mx>
2025-11-24 14:50:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from tts_engine import synthesize
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # or ["https://siajnayarit.gob.mx:86"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TTSRequest(BaseModel):
     text: str
